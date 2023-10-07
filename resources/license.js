fb2cc309159d0724bfcf02d4363ba1d4aa60f9cc
Lucas Gutierrez <lucasgutie@hotmail.com>
2023-10-07 09:44:56 -0300

    Cambio de licencia para precommit

--- resources/license.js
+++ resources/license.js
@@ -3,7 +3,7 @@
  * Facultad de Ciencias Exactas y Tecnología, Universidad Nacional de Tucumán
  * https://www.microprocesadores.unt.edu.ar/
  *
- * Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ * Copyright (c) 2023, Lucas Gutierrez <lucasgutie2003@gmail.com>
  *
  * SPDX-License-Identifier: MIT
  */
