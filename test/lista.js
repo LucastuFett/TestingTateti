e5ee153523d025eb85c42514c306accfed95617e
Lucas Gutierrez <lucasgutie@hotmail.com>
2023-10-07 09:42:20 -0300

    Inicializacion de tests de lista

---
+++ test/lista.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023, Laboratorio de Microprocesadores
+ * Facultad de Ciencias Exactas y Tecnología, Universidad Nacional de Tucumán
+ * https://www.microprocesadores.unt.edu.ar/
+ *
+ * Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+const describe = require('mocha').describe
+const it = require('mocha').it
+const assert = require('chai').assert
+
+//const lista = require('../src/lista.js')
+
+describe('En una lista pares clave:valor ...', function () {
+  describe('Cuando la lista está vacía ...', function () {
+    it('hay cero elementos',function(){
+        assert.fail('empezamos')
+    })
+  })
+})
